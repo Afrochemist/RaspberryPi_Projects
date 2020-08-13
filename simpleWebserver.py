0b9c0f1908b0f7690ab11a393721bd2392b6b752
Afrochemist <yamaha366@aol.com>
2020-08-13 13:58:23 -0400

    Simple webserver

---
+++ simpleWebserver.py
@@ -0,0 +1,12 @@
+#This is a simple webserver
+
+#neccessary packages 
+from flask import Flask 
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+	return 'This is a webserver'
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=80, debug = True)
