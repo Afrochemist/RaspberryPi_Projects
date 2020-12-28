a7d1dc02d68a45a4e2a29f00417f4eb56b050f1b
Allan Brooks <yamaha366@aol.com>
2020-12-28 07:32:18 -0500

    Create moneroscript.sh
    a scripts that mines monero

---
+++ moneroscript.sh
@@ -0,0 +1,10 @@
+#This script mines monero on the raspberry pi
+
+cd /opt/cpuminer-multi
+
+#the code below are scripts
+#./autogen.sh
+#./configure
+#./build.sh
+
+#The line below mines monero and sends it to a pool in minergate.com
