15b5ae0d17b57b455b505742cabf07a8df446231
Allan Brooks <yamaha366@aol.com>
2021-01-04 22:32:57 -0500

    Added the code that initiates the monero mining

--- moneroscript.sh
+++ moneroscript.sh
@@ -2,9 +2,10 @@
 
 cd /opt/cpuminer-multi
 
-#the code below are scripts
+#the code below are scripts compiles codes and needs to be ran once
 #./autogen.sh
 #./configure
 #./build.sh
 
 #The line below mines monero and sends it to a pool in minergate.com
+./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u <email address>
