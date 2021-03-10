e5d70c92d7103433d5d2c9efe0d0cba0e1420df8
Afrochemist <Afrochemist@gmail.com>
2021-03-09 20:12:04 -0500

    added the neccessary packages and code for the api

--- twitterBot.py
+++ twitterBot.py
@@ -4,3 +4,20 @@
 #sudo apt-get install python-setuptools
 #sudo pip install twython
 
+#Necessary packages
+import random
+import os
+import sys
+from twython import Twython
+
+#variables for API 
+CONSUMER_KEY =
+CONSUMER_SECRET =
+ACCESS_KEY =
+ACCESS_SECRET = 
+
+#API setup
+api = Twython(CONSUMER_KEY, CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+
+
+api.update_status(status=sys.argv[1])
